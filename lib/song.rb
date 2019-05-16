78e5db614e83626619629186b44409eb2503389f
Chet  Chopra <chopra.chet@gmail.com>
2019-05-16 19:11:10 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,61 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create 
+    song = Song.new
+    @@all << song
+    return song
+  end
+  
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    @@all << song
+    return song
+  end
+  
+  def self.create_by_name(name)
+    self.new_by_name(name)
+  end
+  
+  def self.find_by_name(name)
+    @@all.each do |song|
+      if song.name == name
+        return song
+      end
+    end
+    return nil
+  end
+  
+  def self.find_or_create_by_name(name)
+    find_by_name(name) ? find_by_name(name) : create_by_name(name)
+  end
+  
+  def self.alphabetical
+    return @@all.sort_by {
+      |song|
+      song.name
+    }
+  end
+  
+  def self.new_from_filename(filename)
+    split_filename = filename.split(/\.|\-/)
+    song = Song.new
+    song.name = split_filename[1].strip
+    song.artist_name = split_filename[0].strip
+    @@all << song
+    return song
+  end
+  
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename)
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
 end
+
+# Song.new_from_filename("Thundercat - For Love I Come.mp3")
+
