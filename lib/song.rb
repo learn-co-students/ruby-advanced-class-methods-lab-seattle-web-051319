c204386cc018ef6079dd401047d76263b6b0e3b0
AustinBH <harlowaustin2@gmail.com>
2019-05-16 13:34:18 -0700

    Done

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +9,83 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    named_song = Song.create
+    named_song.name = name
+    named_song
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|song| song.to_s.include?(name)}
+  end
+
+  def self.find_or_create_by_name(name)
+    if Song.find_by_name(name) == nil
+      Song.create_by_name(name)
+    else
+      Song.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+
+    splitter = filename.split(" - ")
+    split_two = splitter[1].split(".")
+
+    song = Song.new
+
+    song.artist_name = splitter[0]
+    if split_two.length == 2
+      song.name = split_two[0]
+    else
+      song.name = split_two[0..split_two.length-2].join(".")
+    end
+
+    song
+  end
+
+  def self.create_from_filename(filename)
+
+    splitter = filename.split(" - ")
+    split_two = splitter[1].split(".")
+
+    song = Song.create
+
+    song.artist_name = splitter[0]
+    if split_two.length == 2
+      song.name = split_two[0]
+    else
+      song.name = split_two[0..split_two.length-2].join(".")
+    end
+
+    song
+
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def to_s
+    "#{@name}"
   end
 
 end
